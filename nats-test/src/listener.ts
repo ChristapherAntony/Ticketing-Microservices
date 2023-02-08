82bee9b3a2b19f14f659d80f54cef6179e22dba4
Christapher Antony <christapher012@gmail.com>
2023-02-08 21:29:45 +0530

    11. Queue Groups

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,25 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});  //connection
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group'   //queue group
+  );   //subscription created with channel subject name
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,7 @@
-import nats from 'node-nats-streaming';  
+import nats from 'node-nats-streaming';
+
+console.clear();
+
 
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
@@ -6,4 +9,14 @@ const stan = nats.connect('ticketing', 'abc', {
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });   //need to strigfy while sending
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
 });
